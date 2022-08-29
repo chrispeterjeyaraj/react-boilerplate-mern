01b49a9cc509355f298ae875c63fd01baa6d4660
christopherpeter <mailtochrisnow@gmail.com>
2022-08-29 16:16:13 +0530

    removed unused code

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,13 +2,10 @@ import * as React from "react";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import { Link as RouterLink } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
 
 export const LoginPage = () => {
